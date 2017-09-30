30f3c745bee04d85ab004dbdb6da4e6d267d3993
future88075620 <future870914@gmail.com>
2017-09-30 15:03:41 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int x,y,z;
+	    x=in.nextInt();
+	    y=in.nextInt();
+        z=in.nextInt();
+        System.out.printf("%d\n",x+y+z);
+        System.out.printf("%d\n",(x+y+z)/3);
+
+    }
+}
